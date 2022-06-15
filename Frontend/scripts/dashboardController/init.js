82d765ece3699da0ee7d31e731ff8e93b50423d8
AndreiCautisanu <andrei.cautisanu@gmail.com>
2022-06-15 15:10:26 +0300

    dashboard & docs setup

---
+++ Frontend/scripts/dashboardController/init.js
@@ -0,0 +1,21 @@
+const userData = JSON.parse(localStorage.getItem('userData'));
+const userName = userData.username;
+
+// get greeting depending on time of day
+const getGreeting = () => {
+	const currentTime = new Date().getHours();
+	let greeting;
+
+	if (currentTime < 12) {
+		greeting = 'Good morning';
+	} else if (currentTime < 18) {
+		greeting = 'Good afternoon';
+	} else {
+		greeting = 'Good evening';
+	}
+
+	return greeting;
+};
+
+const greetingElem = document.querySelector('.main-greeting');
+greetingElem.innerHTML = `${getGreeting()}, ${userName}!`;
